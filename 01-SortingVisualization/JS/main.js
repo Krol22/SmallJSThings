f7f7f72aba3e46daaea85f32a92f1c073f6f438a
Torun Karol <karol.torun@impaqgroup.com>
2017-06-01 16:59:59 +0200

    01 - working with qsort

--- 01-SortingVisualization/JS/main.js
+++ 01-SortingVisualization/JS/main.js
@@ -1,54 +1,9 @@
 let i = 0;
-let numberOfElements = 100;
+let j = 0;
+let bubbleSort, insertionSort;
+let numberOfElements = 200;
 let startTime, endTime;
 let finished = false;
-
-class Point {
-    constructor(x, y){
-        this.x = x;
-        this.y = y;
-    }
-
-    draw(){
-        fill(color(this.y, this.y, this.y));
-        rect(this.x * 6, 480, 6, - this.y);
-    }
-}
-
-function generateRandomData(){
-    let data = [];
-    for(let i = 0; i < numberOfElements; i++){
-        let randomNumber = round(random(200));
-        data.push(new Point(i, randomNumber));
-    }
-    return data;
-}
-
-function setup(){
-    let canvas = createCanvas(640, 480);
-    canvas.parent('canvas');
-
-    bubbleSort.init();
-    insertionSort.init();
-    startTime = new Date();
-}
-
-function draw(){
-    if(i < 100){
-        insertionSort.stepSort(i);
-        i++;
-    } else {
-        finished = true;
-    }
-
-    if(finished && !endTime){
-        endTime = new Date();
-        let time = (endTime.getTime() - startTime.getTime()) / 1000;
-        let timeElement = document.createElement('div');
-        timeElement.textContent = time + 's';
-        document.querySelector('#canvas').appendChild(timeElement);
-    }
-}
 
 bubbleSort = {
     name: 'Bubble sort',
@@ -91,21 +46,109 @@ quickSort = {
     init: function(){
         this.data = generateRandomData();
     },
-    stepSort: function(step){
+    stepSort: function(){
+        this.sort();
+        clearCanvas();
+        this.data.forEach((point) => { point.draw(); });
+    },
+    sort: function(left, right){
+        left = left || 0;
+        right = right || this.data.length - 1;
+        let index = this.partition(left, right);
+
+
+        if(left < index - 1){
+            this.sort(left, index - 1);
+        }
+        if(right > index) {
+            this.sort(index, right);
+        }
 
     },
-    choosePivot: function(l, r){
-        return (l + (r - 1)) / 2;
-    },
-    divideData: function(){
+    partition: function(left, right){
+        let pivot = Math.floor((left + right) / 2);
 
+        while (left <= right) {
+            while(this.data[left].y < this.data[pivot].y) { left++; }
+            while(this.data[right].y > this.data[pivot].y) { right--; }
+
+            if(left <= right) {
+                this.swapElements(left, right);
+                left++;
+                right--;
+            }
+        }
+
+        return left;
     },
-    swap: function(data, i, j){
-        let temp = data[i].y;
-        data[i].y = data[j].y;
-        data[j].y = temp;
+    swapElements: function(i, j){
+        let temp = this.data[i].y;
+        this.data[i].y = this.data[j].y;
+        this.data[j].y = temp;
+
+    }
+};
+
+let sortObjects = [quickSort];
+
+class Point {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    draw(){
+        fill(color(this.y, this.y, this.y));
+        rect(this.x * 2, 480, 2, - this.y);
     }
 }
+
+function generateRandomData(){
+    let data = [];
+    for(let i = 0; i < numberOfElements; i++){
+        let randomNumber = round(random(200));
+        data.push(new Point(i, randomNumber));
+    }
+    return data;
+}
+
+function setup(){
+    let canvas = createCanvas(1280, 480);
+    canvas.parent('canvas');
+
+    bubbleSort.init();
+    insertionSort.init();
+    quickSort.init();
+    startTime = new Date();
+}
+
+function draw(){
+    if(i < numberOfElements){
+        if(sortObjects[j].name === 'Quick sort'){
+            i = numberOfElements;
+        }
+        sortObjects[j].stepSort(i);
+        i++;
+    } else {
+        finished = true;
+    }
+
+    if(finished && !endTime){
+        endTime = new Date();
+        let time = (endTime.getTime() - startTime.getTime()) / 1000;
+        let timeElement = document.createElement('div');
+        timeElement.textContent = time + 's - ' + sortObjects[j].name;
+        document.querySelector('#canvas').appendChild(timeElement);
+        if(j < sortObjects.length - 1){
+            finished = false;
+            startTime = new Date();
+            j++;
+            i = 0;
+            endTime = false;
+        }
+    }
+}
+
 
 function clearCanvas(){
     noStroke();
