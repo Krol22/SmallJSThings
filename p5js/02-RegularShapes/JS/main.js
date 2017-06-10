5f65bd19e6b7d8726bcd4c43a2bdccaa501267d6
Krol22 <krol22ee@gmail.com>
2017-06-11 00:35:01 +0200

    02-regular shapes

--- p5js/02-RegularShapes/JS/main.js
+++ p5js/02-RegularShapes/JS/main.js
@@ -1,20 +1,35 @@
+const maxR = 105;
+const minR = 0;
+
 function setup(){
     // setup code goes here
     let canvas = createCanvas(640, 480);
     canvas.parent('canvas');
+    colorMode(HSL, 100);
 }
 
 function draw(){
     // draw code goes here
-    translate(100, 100);
-    drawPolygon(0, 0, 20, 5);
+    translate(150, 100);
+    drawPolygon(0, 0, 12, 3);
+    drawPolygon(0, 0, 20, 4);
+    drawPolygon(0, 0, 30, 5);
+    drawPolygon(0, 0, 45, 6);
+    drawPolygon(0, 0, 60, 7);
+    drawPolygon(0, 0, 75, 8);
+    drawPolygon(0, 0, 90, 9);
+    drawPolygon(0, 0, 105, 10);
 }
 
 function drawPolygon(Sx, Sy, r, n){
-    let angle = ((n - 2) * PI) / n;
-    for(let alpha = 0; alpha < TWO_PI; alpha += angle){
+    let angle = TWO_PI / n;
+    stroke(color(map(r, 0, maxR, 0, 100), 50, 50));
+    strokeWeight(1);
+    for(let alpha = (-(PI) / n) + (PI / 2); alpha < TWO_PI - (-(PI) / n) + (PI / 2); alpha += angle){
         let x = Sx + r * cos(alpha);
         let y = Sy + r * sin(alpha);
-        rect(x, y, 5, 5);
+        let x2 = Sx + r * cos(alpha + angle);
+        let y2 = Sy + r * sin(alpha + angle);
+        line(x, y, x2, y2);
     }
 }
